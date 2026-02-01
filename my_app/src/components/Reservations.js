1dd35562f9b8325479597dfea2c1c911c355c3a8
liannamyapp <ardaeducation2025@gmail.com>
2026-02-01 21:01:51 +0400

    Update Reservations.js

--- my_app/src/components/Reservations.js
+++ my_app/src/components/Reservations.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-const API_URL = "https://cafearmenia.onrender.com/api/reservations";
+const API_URL = "https://cafearmenia.onrender.com";
 export default function Reservations() {
     const [formData, setFormData] = useState({
         name: "",
